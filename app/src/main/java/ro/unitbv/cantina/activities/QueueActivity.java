dd2b1ec5ecb33f5bb09f15e4be6a146d31abdfba
iosifnicolae2 <iosifnicolae2@gmail.com>
2018-04-11 21:11:35 +0300

    Send request to the server

--- app/src/main/java/ro/unitbv/cantina/activities/QueueActivity.java
+++ app/src/main/java/ro/unitbv/cantina/activities/QueueActivity.java
@@ -1,33 +1,27 @@
 package ro.unitbv.cantina.activities;
+
 import android.annotation.SuppressLint;
-import android.provider.Settings;
-import android.support.design.widget.Snackbar;
-import java.io.IOException;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import ro.unitbv.cantina.R;
 
 public class QueueActivity extends AppCompatActivity {
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,51 +36,49 @@ public class QueueActivity extends AppCompatActivity {
 
         setTitle(R.string.coada_cantina);
 
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+
+        // Setup click listeners for buttons.
         final Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Integer button1 = 10;
-
+                push_queue_estimation_to_server(10);
             }
         });
         final Button button2 = (Button) findViewById(R.id.button12);
-        button1.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
-                Integer button12 = 20;
+                push_queue_estimation_to_server(20);
             }
         });
         final Button button3 = (Button) findViewById(R.id.button23);
-        button1.setOnClickListener(new View.OnClickListener() {
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
-                Integer button23 = 30;
-            }
-        });
-        final Button button4 = (Button) findViewById(R.id.button34);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
-                Integer button23 = 30;
-            }
-        });
-        final Button button5 = (Button) findViewById(R.id.button45);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
                 push_queue_estimation_to_server(30);
             }
         });
-        final Button button6 = (Button) findViewById(R.id.button56);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button4 = (Button) findViewById(R.id.button34);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
+                push_queue_estimation_to_server(40);
+            }
+        });
+        final Button button5 = (Button) findViewById(R.id.button45);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                push_queue_estimation_to_server(50);
+            }
+        });
+        final Button button6 = (Button) findViewById(R.id.button56);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                push_queue_estimation_to_server(60);
             }
         });
 
@@ -107,19 +99,25 @@ public class QueueActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-//        AndroidNetworking.post("https://unitbv.mailo.ml/api/queue/waze_clients")
-//                .addJSONObjectBody(jsonObject)
-//                .setPriority(Priority.MEDIUM)
-//                .build()
-//                .getAsJSONArray(new JSONArrayRequestListener() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        Log.w("response", response.toString());
-//                    }
-//                    @Override
-//                    public void onError(ANError error) {
-//                        // handle error
-//                    }
-//                });
+        AndroidNetworking.post("https://unitbv.mailo.ml/api/queue/waze_clients")
+                .addJSONObjectBody(jsonObject)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.w("response", response.toString());
+                        // TODO(gabriel): check for errors inside `response` variable.
+                        Snackbar.make(coordinatorLayout, R.string.queue_thanks, Snackbar.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                        anError.printStackTrace();
+                        // TODO(gabriel): show an error message
+                    }
+                });
     }
 }
