61fc77a5c8c115840e9d02fdde8efccd53a11964
iosifnicolae2 <iosifnicolae2@gmail.com>
2018-05-12 10:36:15 +0300

    #1 Remove unused code

--- app/src/main/java/ro/unitbv/cantina/activities/MainActivity.java
+++ app/src/main/java/ro/unitbv/cantina/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package ro.unitbv.cantina.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -11,7 +9,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.ArrayList;
 
@@ -38,15 +35,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         toolbar_a = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar_a);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         fragment_manager = getSupportFragmentManager();
 
@@ -80,10 +68,6 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if(id  == R.id.nav_menu) {
-            setMenuView();
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
